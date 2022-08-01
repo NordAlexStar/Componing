5e92c47f8cdbfbaa219b20da2cf9160a484c3820
Aleksejs Semjonovs <yelowcat@gmail.com>
2022-08-01 18:36:30 +0300

    Buttons

--- Canvas/MainWindow.xaml.cs
+++ Canvas/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls.Primitives;
 
 namespace Canvas
 {
@@ -10,6 +11,30 @@ namespace Canvas
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TheButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBlock != null)
+                textBlock.Text = ((ToggleButton)sender).IsChecked.ToString();
+        }
+
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is not null)
+            { 
+                textBlock1.Text = "Clicked";
+               
+            }
+        }
+
+        private void RepeatButton_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (sender is not null)
+            {
+                textBlock1.Text = "";
+
+            }
         }
     }
 }
