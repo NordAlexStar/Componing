083360508a066b43c06398f640394c522a29ed31
Aleksejs Semjonovs <yelowcat@gmail.com>
2022-08-01 21:04:24 +0300

    next

--- Canvas/MainWindow.xaml.cs
+++ Canvas/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 
 namespace Canvas
@@ -13,18 +14,14 @@ namespace Canvas
             InitializeComponent();
         }
 
-        private void TheButton_Checked(object sender, RoutedEventArgs e)
-        {
-            if (textBlock != null)
-                textBlock.Text = ((ToggleButton)sender).IsChecked.ToString();
-        }
 
         private void RepeatButton_Click(object sender, RoutedEventArgs e)
         {
             if (sender is not null)
-            { 
-                textBlock1.Text = "Clicked";
-               
+            {
+
+                ((FrameworkElement)((ContentControl)sender).Content).Visibility = Visibility.Visible;
+
             }
         }
 
@@ -32,7 +29,7 @@ namespace Canvas
         {
             if (sender is not null)
             {
-                textBlock1.Text = "";
+                ((FrameworkElement)((ContentControl)sender).Content).Visibility = Visibility.Collapsed;
 
             }
         }
