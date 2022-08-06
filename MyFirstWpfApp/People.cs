8795ed21882aed86a0b2be3042c8dcab8a131cd2
Aleksejs Semjonovs <yelowcat@gmail.com>
2022-08-06 12:19:20 +0300

    fix

--- MyFirstWpfApp/People.cs
+++ MyFirstWpfApp/People.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyFirstWpfApp
 {
-    public class People
+    public class People : INotifyPropertyChanged
     {
-        public string? Name { get; set; }
+        private string? name;
+        public string? Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string? family;
+        public string? Family
+        {
+            get
+            {
+                return family;
+            }
+            set
+            {
+                family = value;
+                OnPropertyChanged();
+            }
+        }
 
         public override string ToString()
         {
@@ -19,6 +47,17 @@ namespace MyFirstWpfApp
         {
             Name = "Unknown",
         };
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
 
     }
 }
