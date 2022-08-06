075326a7f7cdd7f854126ae4ce264a2d220c486f
Aleksejs Semjonovs <yelowcat@gmail.com>
2022-08-06 11:49:56 +0300

    update

--- Stackpanel/MainWindow.xaml.cs
+++ Stackpanel/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+using MyFirstWpfApp;
 
 namespace Stackpanel
 {
@@ -7,9 +11,26 @@ namespace Stackpanel
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            People TheBoy = (People)Resources["Vasja"];
+            TheBoy.Name = "Pjotrs";
+        }
+        
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           
         }
     }
 }
