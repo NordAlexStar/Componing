5e2be3350ed2736efafeec43676d28c5912bfd40
Aleksejs Semjonovs <yelowcat@gmail.com>
2022-08-08 18:59:02 +0300

    Update

--- Stackpanel/MainWindow.xaml.cs
+++ Stackpanel/MainWindow.xaml.cs
@@ -11,26 +11,31 @@ namespace Stackpanel
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+
         public MainWindow()
         {
+
             InitializeComponent();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            
+            DataContext = new StackpanelViewModel()
+            {
+                FirstPeople = new People() { Family = "Pupkin", Name = "Vasja" },
+                SecondPeople = new People() { Name = "Aleksejs", Family = "Semjonovs" }
+            };
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            People TheBoy = (People)Resources["Vasja"];
-            TheBoy.Name = "Pjotrs";
+
         }
-        
+
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-           
+
         }
     }
 }
---
+++ Stackpanel/StackpanelViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using MyFirstWpfApp;
+
+namespace Stackpanel
+{
+    public class StackpanelViewModel : INotifyPropertyChanged
+    {
+      
+        public People? FirstPeople
+        {
+            get
+            {
+                return people;
+            }
+            set
+            {
+                people = value;
+                OnPropertyChanged();
+            }
+        }
+       
+        public People? SecondPeople
+        {
+            get
+            {
+                return people2;
+            }
+            set
+            {
+                people2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #region MyRegion
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+
+        private People? people;
+        private People? people2;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        } 
+        #endregion
+    }
+}
